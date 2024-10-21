dcc682cc642d1f940b679034bd6b0aa9abd5dbb7
Danaj66 <64786952+Danaj66@users.noreply.github.com>
2024-10-21 17:08:58 +0200

    Create main.py
    Naredil začetni meni za navigacijo po programu

---
+++ main.py
@@ -0,0 +1,22 @@
+# main.py
+
+def main():
+
+    while True:
+        print("\n1. Dodaj opravilo\n2. Seznam opravil\n3. Brisi opravilo\n4. Izhod")
+        choice = input("Vnesite izbiro: ")
+
+        if choice == '1':
+            task = input("Vnesite opravilo: ")
+        elif choice == '2':
+            break
+        elif choice == '3':
+            task_id = int(input("Vnesite stevilo opravila, ki ga hocete zbrisati: "))
+        elif choice == '4':
+            print("Izhod...")
+            break
+        else:
+            print("Napacna izbira! Poskusite ponovno.")
+
+if __name__ == "__main__":
+    main()
